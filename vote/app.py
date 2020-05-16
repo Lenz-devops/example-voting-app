02b9adac16d4b7c6bc7f793b5cb7bf717dd74743
Lenz-devops <florent.lauber@gmail.com>
2020-05-16 12:38:43 +0200

    changes added to app.py

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
-redis_password = os.getenv('REDIS_PASSWORD', "Dogs")
+option_b = os.getenv('OPTION_B', "Ugly Dogs")
+redis_password = os.getenv('REDIS_PASSWORD', "Cats")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
