443c38f13d16e76071347e113650e2e930547fa5
Lenz-devops <florent.lauber@gmail.com>
2020-05-16 13:10:41 +0200

    changes added to server.js

--- result/server.js
+++ result/server.js
@@ -12,7 +12,7 @@ var express = require('express'),
 
 io.set('transports', ['polling']);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 4000;
 
 io.sockets.on('connection', function (socket) {
 
@@ -23,14 +23,15 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-var pool = new pg.Pool({
-  connectionString: 'postgres://postgres_user:postgres_password@db/postgres'
-});
+// var pool = new pg.Pool({
+//   connectionString: 'postgres://postgres_user:postgres_password@db/postgres'
+// });
 
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
-    pool.connect(function(err, client, done) {
+    // pool.connect(function(err, client, done) {
+    pg.connect('postgres://postgres_user:postgres_password@db/postgres', function(err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
